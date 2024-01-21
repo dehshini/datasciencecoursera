417b7098ba6054cd1fbb55379d961013bef4fa7c
dehshini <alidehshini@outlook.com>
2024-01-21 15:18:58 -0500

    plot1 code and image added

--- exploratory_analysis/plot1.r
+++ exploratory_analysis/plot1.r
@@ -4,8 +4,8 @@ library(tidyverse)
 
 #download the zip file
 # download.file(
-    #"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
-    #"./exploratory_analysis/household_power_consumption.zip"
+    # "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+    #destfile = "./exploratory_analysis/household_power_consumption.zip"
 )
 
 #unzip the file
@@ -13,7 +13,7 @@ library(tidyverse)
 
 #read the data
 data <- read.table(
-    "household_power_consumption.txt",
+    "./datasets/household_power_consumption.txt",
     sep = ";",
     header = TRUE,
     na.strings = "?"
@@ -23,7 +23,19 @@ data <- read.table(
 data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
 
 #filter the data
-data <- data %>%
+data1 <- data %>%
     filter(
         Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02")
     )
+
+#plot the data
+#png("plot1.png", width = 480, height = 480)
+
+png("./exploratory_analysis/plot1.png", width = 480, height = 480)
+hist(
+    data1$Global_active_power,
+    col = "red",
+    main = "Global Active Power",
+    xlab = "Global Active Power (kilowatts)"
+)
+dev.off()
