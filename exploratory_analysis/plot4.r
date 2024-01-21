4aa1472f3c1088d58164909acd565c06f83b9d35
dehshini <alidehshini@outlook.com>
2024-01-21 17:25:41 -0500

    plot4 code and image

--- exploratory_analysis/plot4.r
+++ exploratory_analysis/plot4.r
@@ -27,3 +27,42 @@ data1 <- data %>%
 datetime <- paste(as.Date(data1$Date), data1$Time)
 data1$Datetime <- as.POSIXct(datetime)
 
+#plot the graph
+par(mfrow = c(2, 2))
+with(data1, {
+    plot(
+        Global_active_power ~ Datetime,
+        type="l",
+        ylab="Global Active Power (kilowatts)",
+        xlab=""
+    )
+    plot(
+        Voltage ~ Datetime,
+        type="l",
+        ylab="Voltage (volt)",
+        xlab="datatime"
+    )
+    plot(
+        Sub_metering_1 ~ Datetime,
+        type="l",
+        ylab="Global Active Power (kilowatts)",
+        xlab="")
+    lines(Sub_metering_2 ~ Datetime, col='Red')
+    lines(Sub_metering_3 ~ Datetime, col='Blue')
+    legend(
+        "topright",
+        col = c("black", "red", "blue"),
+        lty = 1,
+        lwd = 2,
+        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+    )
+    plot(
+        Global_reactive_power ~ Datetime,
+        type="l",
+        ylab="Global_Rective_Power",
+        xlab="datetime"
+    )
+}
+)
+
+dev.print(png, file = "./exploratory_analysis/plot4.png", width = 480, height = 480)
