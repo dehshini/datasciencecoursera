8290d01259ab1caead1316e6c814b30347a89ba8
dehshini <alidehshini@outlook.com>
2024-01-21 16:53:22 -0500

    plot3 image and code

--- exploratory_analysis/plot3.r
+++ exploratory_analysis/plot3.r
@@ -26,10 +26,28 @@ data1 <- data %>%
         Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02")
     )
 
+datetime <- paste(as.Date(data1$Date), data1$Time)
+data1$Datetime <- as.POSIXct(datetime)
+
 #plot the data
-plot(
-    data1$Global_active_power,
-    type = "l",
-    xlab = "",
-    ylab = "Global Active Power (kilowatts)"
+with(data1, {
+    plot(
+        Sub_metering_1 ~ Datetime,
+        type = "l",
+        ylab = "Global Active Power (kilowatts)",
+        xlab = ""
+        )
+    lines(Sub_metering_2 ~ Datetime, col = "Red")
+    lines(Sub_metering_3 ~ Datetime, col = "Blue")
+    }
 )
+
+legend(
+    "topright",
+    col = c("black", "red", "blue"),
+    lty = 1,
+    lwd = 2,
+    legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+)
+
+dev.print(png, file = "./exploratory_analysis/plot3.png", width = 480, height = 480)
